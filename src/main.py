50d64e2ca1bd0ac53854a528c6280c3b9301b839
NaufalAugustMay <naufalaugust.2025@student.uny.ac.id>
2026-05-16 15:12:40 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -64,18 +64,45 @@ class Graph:
 
     def add_node(self, node_id: str, nama: str) -> None:
         """Big-O: O(1)."""
-        # TODO: implementasikan
-        pass
+         if node_id not in self.adj:
+            self.adj[node_id] = None
+            self.node_names[node_id] = nama
 
     def add_edge(self, u: str, v: str, bobot: int) -> None:
         """Big-O: O(1) tambah di head linked list."""
-        # TODO: implementasikan (graf tidak berarah: dua arah)
-        pass
+        # tambah edge u -> v
+        new_node_v = EdgeNode(v, bobot)
+        new_node_v.next = self.adj[u]
+        self.adj[u] = new_node_v
+
+        # tambah edge v -> u (graph tidak berarah)
+        new_node_u = EdgeNode(u, bobot)
+        new_node_u.next = self.adj[v]
+        self.adj[v] = new_node_u
 
     def neighbors(self, u: str) -> List[Tuple[str, int]]:
         """Big-O: O(deg(u))"""
-        # TODO: kembalikan list (dest, bobot)
-        pass
+        
+        result = []
+        current = self.adj[u]
+
+        while current is not None:
+            result.append((current.dest, current.bobot))
+            current = current.next
+
+        return result
+
+    def is_connected(self, u: str, v: str) -> bool:
+        """Cek apakah ada edge langsung antara u dan v."""
+        
+        current = self.adj[u]
+
+        while current is not None:
+            if current.dest == v:
+                return True
+            current = current.next
+
+        return False
 
 # — Dijkstra (implementasikan dari sini) —
 def dijkstra(graph: Graph, source: str) -> Tuple[Dict[str, int],
